05842a2347cd2cfed80ba50dd7307fde5f2a41fd
amilaupeksha <amilaupeksha@outlook.com>
2014-09-29 14:14:56 +0530

    implementation of classe

---
+++ TestQuartz/TestQuartz/EmailJob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace TestQuartz
+{
+    /// <summary>
+    /// For email job
+    /// </summary>
+   public class EmailJob:IJob
+    {
+        /// <summary>
+        /// Excutes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+       public void Excute(IJobExecutionContext  context) 
+       {
+           // TODO: Implement this
+           throw new System.NotImplementedException();
+       }
+    }
+}
--- TestQuartz/TestQuartz/Program.cs
+++ TestQuartz/TestQuartz/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
 
 namespace TestQuartz
 {
-    class Program
+     public class Program
     {
-        static void Main(string[] args)
+         private static readonly ISchedulerFactory SchedulerFactory;
+         private static readonly IScheduler Scheduler;
+
+
+         /// <summary>
+         /// Initializes the <see cref="Program"/> class.
+         /// </summary>
+         static Program()
+         {
+             SchedulerFactory = new SchedulerFactory();
+             Scheduler = SchedulerFactory.GetScheduler();
+         }
+
+         /// <summary>
+         /// Defines the entry point of the application.
+         /// </summary>
+         /// <param name="args">The arguments.</param>
+         static void Main(string[] args)
         {
+            Scheduler.Start();
+            
         }
+
+         private static void CreatJob() 
+         {
+          _emailJobDetail =JobBuilder.Create
+         }
     }
 }
